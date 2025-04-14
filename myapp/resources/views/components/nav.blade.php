c6b75054aa5b2f78a24f89a86ebd020b57f89050
VialsShiny <vialsvialatou@gmail.com>
2025-04-14 16:10:29 +0200

    Finishing US | Starting US001-C

---
+++ myapp/resources/views/components/nav.blade.php
@@ -0,0 +1,34 @@
+<nav>
+    <ul class="flex justify-between items-center">
+        <div>
+            <a href="/" class="flex gap-3 items-center">
+                <x-app-logo />
+                <li class="font-bold">
+                    Car Rental
+                </li>
+            </a>
+        </div>
+        <div class="flex gap-6">
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Vehicules</a>
+            </li>
+            <li>
+                <a href="#">Details</a>
+            </li>
+            <li>
+                <a href="#">About Us</a>
+            </li>
+        </div>
+        <div class="flex items-center gap-3">
+            <i
+                class="flex ri-phone-fill text-white text-2xl justify-center items-center min-w-12 min-h-12 rounded-full bg-[#5937E0]"></i>
+            <li>
+                <p>Need Help ?</p>
+                <strong>+537 547-6401</strong>
+            </li>
+        </div>
+    </ul>
+</nav>
