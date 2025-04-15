199c61b2154d81027e79cc16a7baa5b5011210bb
VialsShiny <vialsvialatou@gmail.com>
2025-04-15 14:54:13 +0200

    All Vehicules DONE | US002-A M DONE

--- myapp/resources/views/vehicules.blade.php
+++ myapp/resources/views/vehicules.blade.php
@@ -1,3 +1,18 @@
 <x-app-layout :title="$title">
-  <!-- Véhicules todo -->
+    <div class="flex flex-wrap justify-center gap-4">
+      @foreach ($vehicules as $vehicule)
+          <x-car-card 
+              id="{{ $vehicule->id }}" 
+              image="{{ strtolower($vehicule->brand) }}" 
+              image_alt="Image of {{ $vehicule->brand }}" 
+              name="{{ $vehicule->brand }}" 
+              model="{{ $vehicule->model }}" 
+              price="{{ $vehicule->price_per_day }}" 
+              type="{{ $vehicule->vehiculeType->name }}" 
+              transmission="{{ $vehicule->transmission }}" 
+              fuel="{{ $vehicule->fuel_type }}" 
+              air_conditioning="{{ $vehicule->air_conditioning }}" 
+          />
+      @endforeach
+    </div>
 </x-app-layout>
