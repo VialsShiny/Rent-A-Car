e0baed09b91668ee6770875c91cfa5e9d7c3555c
VialsShiny <vialsvialatou@gmail.com>
2025-04-15 17:55:56 +0200

    Finishing the two US

--- myapp/resources/views/components/car-presentation.blade.php
+++ myapp/resources/views/components/car-presentation.blade.php
@@ -1,12 +1,12 @@
-@props(['name' => 'Not Found', 'price_per_day' => 'E', 'car_image' => 'undefined.png', 'images' => false])
+@props(['name' => 'Not Found', 'price_per_day' => 'E', 'car_image' => 'undefined.png', 'images' => false, 'customClass' => ''])
 
-<div class="flex flex-col gap-6">
+<div {{ $attributes->merge(['class' => 'flex flex-col gap-6 ' . $customClass]) }}>
     <h2 class="text-4xl font-bold">{{ $name }}</h2>
     <div class="flex gap-2">
-        <strong class="text-[#5937E0] text-2xl">${{ $price_per_day }}</strong>
+        <strong class="text-[#5937E0] text-3xl">${{ $price_per_day }}</strong>
         <p class="font-thin text-sm mt-1">/ day</p>
     </div>
-    <img src="{{ asset('img/img-card') . "/$car_image" }}" alt="tkt" class="blur-sm w-full">
+    <img src="{{ asset('img/img-card') . "/$car_image" }}" alt="tkt" class="blur-sm w-full max-w-[1100px] ">
     <div class="w-full flex gap-6">
         @if (!$images)
             <p class="w-full flex justify-center font-thin">No much images found for this car.</p>
--- myapp/resources/views/reservation.blade.php
+++ myapp/resources/views/reservation.blade.php
@@ -1,3 +1,25 @@
 <x-app-layout :title="$title">
-  <!-- Reservation TODO -->
+    <h1 class="text-5xl font-bold text-center py-12">Reservation</h1>
+
+    <div class="mt-24 grid grid-cols-3 items-center gap-24">
+        <x-car-presentation name="{{ $vehicule->brand }}" price_per_day="{{ $vehicule->price_per_day }}"
+            car_image="{{ $car_image }}" customClass="col-span-2 gap-4" />
+        <form action="#" method="post" class="flex flex-col gap-10">
+            <input type="date" name="start_date" id="start_date"
+                class="w-full border-none rounded-xl bg-gray-300/30 focus:ring-2 focus:ring-black">
+            <input type="date" name="start_date" id="start_date"
+                class="w-full border-none rounded-xl bg-gray-300/30 focus:ring-2 focus:ring-black">
+            <input type="email" name="email" id="email"
+                class="w-full border-none rounded-xl bg-gray-300/30 focus:ring-2 focus:ring-black" placeholder="Email">
+
+            <label for="total_price" class="flex gap-4 items-center font-thin">
+                <input type="checkbox" name="total_price" id="total_price"
+                    class="rounded-full flex justify-center items-center h-6 w-6">
+                Total Price
+            </label>
+
+            <input type="submit"
+                class="focus:outline-none text-white text-center bg-[#5937E0] hover:brightness-75 font-medium rounded-lg text-sm w-2/3 px-5 py-2.5 mb-2 transition-all ease-in-out duration-300" value="Book Now">
+        </form>
+    </div>
 </x-app-layout>
